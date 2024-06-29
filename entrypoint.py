46c6b054e3a82837422313c5a10c9c9997182f12
bantoinese83 <b.antoine.se@gmail.com>
2024-06-29 16:55:14 -0400

    Connected Flask app to Redis using Docker Compose

--- entrypoint.py
+++ entrypoint.py
@@ -1,17 +1,32 @@
-# import the flask web framework
-from flask import Flask
+import json
+import redis as redis
+from flask import Flask, request
+from loguru import logger
 
-# create a Flask server, and allow us to interact with it using the app variable
+HISTORY_LENGTH = 10
+DATA_KEY = "engine_temperature"
+
 app = Flask(__name__)
 
 
-# define an endpoint which accepts POST requests, and is reachable from the /record endpoint
 @app.route('/record', methods=['POST'])
 def record_engine_temperature():
-    # every time the /record endpoint is called, the code in this block is executed
-    pass
+    payload = request.get_json(force=True)
+    logger.info(f"(*) record request --- {json.dumps(payload)} (*)")
 
-    # return a json payload, and a 200 status code to the client
+    engine_temperature = payload.get("engine_temperature")
+    logger.info(f"engine temperature to record is: {engine_temperature}")
+
+    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+    database.lpush(DATA_KEY, engine_temperature)
+    logger.info(f"stashed engine temperature in redis: {engine_temperature}")
+
+    while database.llen(DATA_KEY) > HISTORY_LENGTH:
+        database.rpop(DATA_KEY)
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    logger.info(f"engine temperature list now contains these values: {engine_temperature_values}")
+
+    logger.info(f"record request successful")
     return {"success": True}, 200
 
 
@@ -22,4 +37,4 @@ def collect_engine_temperature():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8000)
